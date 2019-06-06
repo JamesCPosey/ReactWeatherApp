ae5bc348c0ff1fd63d5cb792d93ed3ed0869fe8a
James <james.c.posey@gmail.com>
2019-06-06 14:16:34 -0700

    Styled about page, added info about technology used

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,9 +2,23 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <div>
-      <h3>About</h3>
-      <p>About page placeholder text</p>
+    <div className="text-align: center">
+    <div className="display: inline-block">
+      <h1>About</h1>
+      <p>A React weather application built by James C Posey Jr.</p>
+      <p>Technologies used:</p>
+      <ol>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - The JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - The source API used to search for weather data by city name.
+        </li>
+
+      </ol>
+      
+      <p>Source code: <a href="https://github.com/JamesCPosey/ReactWeatherApp">JamesCPosey's GitHub: React Weather App</a></p>
+    </div>
     </div>
   )
 };
